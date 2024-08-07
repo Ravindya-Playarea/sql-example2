2ca1eee54710288330ee7a2e2737e1c3dd9bd7df
Tharindu Rukshan <tharindurukshan709@gmail.com>
2024-08-07 18:48:10 +0530

    user relation

--- resources/db-scripts.sql
+++ resources/db-scripts.sql
@@ -10,3 +10,15 @@ INSERT INTO customer  (customer_id, name) VALUES
                                               ('C004', 'Ravindaya'),
                                               ('C005', 'Muditha');
 
+CREATE TABLE "user" (
+                        username VARCHAR(100) PRIMARY KEY ,
+                        name VARCHAR(200) NOT NULL ,
+                        password VARCHAR(300) NOT NULL
+);
+
+INSERT INTO "user" (username, name, password) VALUES
+                                                  ('U001','Kasun','kasun123'),
+                                                  ('U002','Nimal','nimal123'),
+                                                  ('U003','Ramesh','ramesh123'),
+                                                  ('U004','Gayan','gayan123'),
+                                                  ('U005','Sadun','sadun123');
