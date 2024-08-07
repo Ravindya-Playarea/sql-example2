5b5bb9f65eec387b45ea3d5a1f0817984ebebd7e
Tharindu Rukshan <tharindurukshan709@gmail.com>
2024-08-07 19:06:39 +0530

    order detail relation

--- resources/db-scripts.sql
+++ resources/db-scripts.sql
@@ -51,3 +51,23 @@ INSERT INTO item(bar_code, stock, description, price) VALUES
                                                           ('B003',10,'Head Set',4000.00),
                                                           ('B004',10,'Lap top',150000.00),
                                                           ('B005',10,'Laptop Cover',3200.00);
+
+CREATE TABLE order_detail(
+                             order_id VARCHAR(10),
+                             bar_code VARCHAR(50),
+                             price DECIMAL(9,2) NOT NULL ,
+                             qty INT NOT NULL ,
+                             CONSTRAINT pk_order_detail PRIMARY KEY (order_id,bar_code),
+                             CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES "order"(order_id),
+                             CONSTRAINT fk_item FOREIGN KEY (bar_code) REFERENCES item(bar_code)
+);
+
+
+
+
+INSERT INTO order_detail (order_id, bar_code, price, qty) VALUES
+                                                              ('OD001','B001',2500.00,2),
+                                                              ('OD002','B002',750.00,1),
+                                                              ('OD003','B003',1500.00,1),
+                                                              ('OD004','B004',150000.00,1),
+                                                              ('OD005','B005',6400.00,2);
